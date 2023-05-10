27ffded78743504ddf28d0e314e1a58ca3fb07d0
Vincent <vincentamoguis@gmail.com>
2023-05-10 17:23:43 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+
+<h1>Student Management System</h1>
+<button>Add Student</button>
+
+</body>
+</html>
+
+<?php
+echo phpversion();
+?>
